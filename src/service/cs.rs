6a364cd0a0ef53ed4cfa6777b88cc8680eddba9e
nurislam-td <ntasbolatov03@gmail.com>
2026-02-28 21:47:15 +0500

    update crypto service encoding/decoding to/from one blob

--- src/service/cs.rs
+++ src/service/cs.rs
@@ -7,9 +7,6 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum CryptoServiceError {
-    #[error("random number generator failed: {0}")]
-    EntropyError(String),
-
     #[error("Key derivation function failed: {0}")]
     KeyDerivationError(String),
 
@@ -18,18 +15,9 @@ pub enum CryptoServiceError {
 
     #[error("Decoding error: {0}")]
     DecodingError(String),
-
-    #[error("Invalid lengths of parameters: {0}")]
-    InvalidLengthError(String),
 }
 
 type CryptoServiceResult<T> = Result<T, CryptoServiceError>;
-
-pub struct EncryptedBundle {
-    pub ciphertext: Vec<u8>,
-    salt: String,
-    nonce: Vec<u8>,
-}
 
 fn derive_key(passphrase: &str, salt: &str) -> CryptoServiceResult<[u8; 32]> {
     let mut key = [0u8; 32];
@@ -39,31 +27,53 @@ fn derive_key(passphrase: &str, salt: &str) -> CryptoServiceResult<[u8; 32]> {
     Ok(key)
 }
 
-pub fn encrypt(password: &str, passphrase: &str) -> CryptoServiceResult<EncryptedBundle> {
+pub fn encrypt(password: &str, passphrase: &str) -> CryptoServiceResult<Vec<u8>> {
     let salt = SaltString::generate(&mut OsRng);
     let key = derive_key(passphrase, &salt.as_str())?;
+
     let cipher = Aes256Gcm::new_from_slice(&key)
         .map_err(|e| CryptoServiceError::EncodingError(e.to_string()))?;
+
     let nonce = Aes256Gcm::generate_nonce(&mut OsRng);
+
     let ciphertext = cipher
         .encrypt(&nonce, password.as_bytes())
         .map_err(|e| CryptoServiceError::EncodingError(e.to_string()))?;
 
-    Ok(EncryptedBundle {
-        ciphertext,
-        salt: salt.to_string(),
-        nonce: nonce.to_vec(),
-    })
+    let salt_bytes = salt.as_str().as_bytes();
+    let mut blob = Vec::new();
+    blob.push(salt_bytes.len() as u8);
+    blob.extend_from_slice(salt_bytes);
+    blob.extend_from_slice(&nonce);
+    blob.extend_from_slice(&ciphertext);
+
+    Ok(blob)
 }
 
-pub fn decrypt(bundle: &EncryptedBundle, passprhase: &str) -> CryptoServiceResult<String> {
-    let key = derive_key(passprhase, &bundle.salt.as_str())?;
+pub fn decrypt(pswd_blob: &Vec<u8>, passphrase: &str) -> CryptoServiceResult<String> {
+    if pswd_blob.is_empty() {
+        return Err(CryptoServiceError::DecodingError(
+            "Empty ciphertext".to_string(),
+        ));
+    }
+
+    let salt_len = pswd_blob[0] as usize;
+    let salt_end = 1 + salt_len;
+    let salt_str = std::str::from_utf8(&pswd_blob[1..salt_end])
+        .map_err(|_| CryptoServiceError::DecodingError("Incorrect salt".into()))?;
+
+    let nonce_start = salt_end;
+    let nonce_end = 12 + nonce_start;
+    let nonce = Nonce::from_slice(&pswd_blob[nonce_start..nonce_end]);
+
+    let ciphertext = &pswd_blob[nonce_end..];
+
+    let key = derive_key(passphrase, salt_str)?;
     let cipher = Aes256Gcm::new_from_slice(&key)
         .map_err(|e| CryptoServiceError::DecodingError(e.to_string()))?;
-    let nonce = Nonce::from_slice(&bundle.nonce);
 
     let decrypted_bytes = cipher
-        .decrypt(nonce, bundle.ciphertext.as_ref())
+        .decrypt(nonce, ciphertext)
         .map_err(|e| CryptoServiceError::DecodingError(e.to_string()))?;
     String::from_utf8(decrypted_bytes).map_err(|e| CryptoServiceError::EncodingError(e.to_string()))
 }
