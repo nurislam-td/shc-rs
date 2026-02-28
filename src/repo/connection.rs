af9530e720804220d3820be3dfcd7329f05d3f6a
nurislam-td <ntasbolatov03@gmail.com>
2026-02-28 21:48:38 +0500

    fix connection repo create list methods

--- src/repo/connection.rs
+++ src/repo/connection.rs
@@ -5,34 +5,35 @@ use crate::db::{
     schema::connection,
 };
 
-pub fn create_connection(conn: &mut SqliteConnection, name: &str, password: &str, host: &str) {
+pub fn create(conn: &mut SqliteConnection, name: &str, password: &Vec<u8>, host: &str) {
     let new_conn = NewConnection {
         name,
         password,
         host,
+        port: 22,
     };
 
     diesel::insert_into(connection::table)
         .values(&new_conn)
         .execute(conn)
-        .expect("Ошибка вставки");
+        .expect("Insert error");
 }
 
 pub fn list_connections(conn: &mut SqliteConnection) -> Vec<SshConnection> {
     connection::table
         .load::<SshConnection>(conn)
-        .expect("Ошибка выборки")
+        .expect("Select error")
 }
 
 pub fn update_name(conn: &mut SqliteConnection, conn_id: i32, new_name: &str) {
     diesel::update(connection::table.find(conn_id))
         .set(connection::name.eq(new_name))
         .execute(conn)
-        .expect("Ошибка обновления");
+        .expect("Update name error");
 }
 
 pub fn delete_connection(conn: &mut SqliteConnection, conn_id: i32) {
     diesel::delete(connection::table.find(conn_id))
         .execute(conn)
-        .expect("Ошибка удаления");
+        .expect("Delete error");
 }
